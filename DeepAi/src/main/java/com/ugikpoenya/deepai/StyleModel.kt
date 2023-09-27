f72bd1ae5dedbf308a15ecbe62fb8960aac2e365
Sugeng Riyanto PC <ugikpoenya@gmail.com>
2023-09-27 17:09:08 +0700

    Style model : Serializable

--- DeepAi/src/main/java/com/ugikpoenya/deepai/StyleModel.kt
+++ DeepAi/src/main/java/com/ugikpoenya/deepai/StyleModel.kt
@@ -1,6 +1,8 @@
 package com.ugikpoenya.deepai
 
-class StyleModel(title: String, image: String, url: String) {
+import java.io.Serializable
+
+class StyleModel(title: String, image: String, url: String) : Serializable {
     var title: String? = title
     var image: String? = image
     var url: String? = url
